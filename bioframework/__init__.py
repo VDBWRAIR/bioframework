9656134eb8e141882552db08de9e3872754489f9
Tyghe Vallard <vallardt@gmail.com>
2016-02-02 11:47:31 -0500

    fixing requirements and such

--- bioframework/__init__.py
+++ bioframework/__init__.py
@@ -1,5 +1,6 @@
 __projectname__ = 'bioframework'
 __release__ = '0.0.1'
 __authors__ = "Tyghe Vallard, Michael Panciera"
+__authoremails__ = 'vallardt@gmail.com, michael.panciera.work@gmail.com'
 __description__ = "Basic building blocks for bio-pipelines"
 __keywords__ = "pyjip, pipeline, bioinformatics"
