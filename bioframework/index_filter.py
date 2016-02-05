1b451d092d01814aed483491f9cb0cbfc3912c32
averagehat <michael.panciera.work@gmail.com>
2016-02-05 11:21:29 -0500

    added interleaved index filter

--- bioframework/index_filter.py
+++ bioframework/index_filter.py
@@ -1,14 +1,15 @@
 from Bio import SeqIO
+from toolz.itertoolz import second, first, partition
 import re
 import os
 from itertools import ifilter, imap, izip
 from toolz import compose
-from toolz.itertoolz import second, first
 from seqio import write_zip_results
 from functools import partial
-def filter_on_index(seqs, index_seqs, predicate):
+
+def filter_on_index(predicate, *seqs):
     pred = compose(predicate, second)
-    return imap(first, ifilter(pred, izip(seqs, index_seqs)))
+    return imap(first, ifilter(pred, izip(*seqs)))
 
 def write_index_filter(input, output, predicate):
     def get_index(path):
@@ -17,7 +18,7 @@ def write_index_filter(input, output, predicate):
     index = get_index(input)
     if index is None:
         raise ValueError("Index %s for file %s not found" % (index, input))
-    return write_zip_results(partial(filter_on_index, predicate=predicate),
+    return write_zip_results(partial(filter_on_index, predicate),
                       output, 'fastq', input, index)
 
 def filter_on_index_quality(input, output, minimum):
@@ -27,3 +28,17 @@ def filter_on_index_quality(input, output, minimum):
     write_index_filter(input, output, below_qual)
     return 0
 
+
+def filter_on_index_quality_interleaved(interleaved, index1, index2, output, minimum):
+    '''enpair the interleaved read file,zip that enpaired with the two indexes
+    drop pairs from the interleaved file if either *index* is below the minimum'''
+    above_min = lambda x: min(x.quality) >= minimum
+    def indexes_above_min(seqs, idx1,idx2):
+        return above_min(idx1) and above_min(idx2)
+    def qual_filter(interleaved, idx1, idx2):
+        pairs = partial(partition, 2)
+        interleaved = pairs(interleaved)
+        zipped = izip(interleaved, idx1, idx2)
+        filtered = imap(first, ifilter(indexes_above_min, zipped))
+        return izip(*filtered)
+    return write_zip_results(qual_filter, output, 'fastq')
