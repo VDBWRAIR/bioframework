2d2efd779fcf9c2957ee5ba9dc891e856bdd0cd9
averagehat <michael.panciera.work@gmail.com>
2016-02-10 11:04:07 -0500

    more tests, all working locally

--- tests/test_filter_hypothesis.py
+++ tests/test_filter_hypothesis.py
@@ -1,6 +1,7 @@
 #TODO: make io test use commandline jip
 #NOTE: this duplicates  a lot of the tested code's logic (see IO, above_min, imports) and API dependencies (see imports)
 from Bio.SeqRecord import SeqRecord
+import os
 import sh
 from Bio.Seq import Seq
 from Bio.Alphabet import IUPAC
@@ -8,7 +9,8 @@ from Bio import SeqIO
 from functools import partial
 import re
 from bioframework.seqio import interleave
-import unittest 
+import unittest
+from itertools import takewhile
 import operator
 from toolz.itertoolz import partition
 import hypothesis
@@ -42,9 +44,6 @@ reads_and_indices = st.integers(min_value=1,max_value=5).flatmap(
 def ilen(seq): return sum(1 for _ in seq)
 
 class TestIndexQualityFilter(unittest.TestCase):
-#    @given(reads_and_indices)
-#    def test_idempotent(self, seqs):
-#        self.assertSequenceEqual(list(qual_filter(*seqs)), list(qual_filter(list(qual_filter(*seqs)), *seqs[1:])))
 
     @given(reads_and_indices)
     def test_always_less_or_equal_size(self, seqs):
@@ -59,7 +58,7 @@ class TestIndexQualityFilter(unittest.TestCase):
         result = qual_filter(*seqs)
         pairs = partition(2, result)
         matches = map(lambda (x,y): x.id == y.id, pairs)
-        self.assertTrue(all(matches)) 
+        self.assertTrue(all(matches))
 
     @given(reads_and_indices)
     def test_drops_all_if_all_lower(self, seqs):
@@ -82,26 +81,43 @@ class TestIndexQualityFilter(unittest.TestCase):
         assume(not all(map(above_min(_min), i1 + i2)))
         result = qual_filter(*seqs)
         self.assertLess(ilen(result), ilen(reads))
-
+    def run_io(self, seqs):
+        names =  ['r', 'i1', 'i2']
+        outfile = 'foo'
+        map(partial(SeqIO.write, format='fastq'), seqs[1:], names)
+        cmd = sh.Command('jip_modules/interleaved_index_filter.jip')
+        cmd(i=names[0], index1=names[1], index2=names[2], minimum=seqs[1], o=outfile) # could use stdout
+        result = SeqIO.parse(outfile, 'fastq')
+        return result
+#
     @given(reads_and_indices)
-    #@hypothesis.settings(max_examples=3)
+    @hypothesis.settings(max_examples=20)
     def test_drops_some_if_any_lower_with_IO(self, seqs):
         (_min, reads, i1, i2) = seqs
         assume(not all(map(above_min(_min), i1 + i2)))
-        names =  ['r', 'i1', 'i2']
-        map(partial(SeqIO.write, format='fastq'), seqs[1:], names)
-        outfile = 'foo'
-        filter_on_index_quality_interleaved(names[0], names[1], names[2], outfile, _min)
-        # version using the jip module itself not working
-        #cmd = sh.Command('jip_modules/interleaved_index_filter.jip')
-        #cmd(i=names[0], index1=names[1], index2=names[2], minimum=_min, o=outfile) # could use stdout
-        result = SeqIO.parse(outfile, 'fastq')
+        result = self.run_io(seqs)
         self.assertLess(ilen(result), ilen(reads))
 
+    @given(reads_and_indices)
+    def test_drops_correct_seq(self, seqs):
+        (_min, reads, i1, i2) = seqs
+        assume(not all(map(above_min(_min), i1)))
+        first_failing_index = ilen(takewhile(above_min(_min), i1))
+        failing_reads = reads[first_failing_index*2:first_failing_index*2+2]
+        result = list(qual_filter(*seqs))
+        self.assertFalse(failing_reads[0] in result or failing_reads[1] in result)
 
+    @given(reads_and_indices)
+    @hypothesis.settings(max_examples=20)
+    def test_drops_correct_seq_IO(self, seqs):
+        (_min, reads, i1, i2) = seqs
+        assume(not all(map(above_min(_min), i1)))
+        first_failing_index = ilen(takewhile(above_min(_min), i1))
+        failing_reads = reads[first_failing_index*2:first_failing_index*2+2]
+        result = self.run_io(seqs)
+        self.assertFalse(failing_reads[0] in result or failing_reads[1] in result)
 if __name__ == '__main__':
     unittest.main()
 # index files should be unchanged
 # the number of reads left should be original - number of bad reads in each index which don't overlap
-# can use find(strategy, boolean) 
-#_not = lambda f: lambda x: not f(x)
+# can use find(strategy, boolean)
