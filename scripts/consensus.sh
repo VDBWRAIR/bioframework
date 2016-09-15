09fb5148f48c1ef341b0378409c0c0a854e581e1
Panciera <michael.panciera@amedrhcwrair001.amed.ds.army.mil>
2016-09-15 14:24:40 -0400

    fix so consensus includes reference as argument to match Makefile

--- scripts/consensus.sh
+++ scripts/consensus.sh
@@ -1,9 +1,9 @@
-if [ -z  "$1" ]; 
+if [ -z  "$1" ] || [ -z "$2" ]; 
 then 
-    echo "Usage: $(basename $0) <ngsdir>";
+    echo "Usage: $(basename $0) <ngsdir> <reference>";
     exit 1
 fi;
 makefile=/media/VD_Research/Admin/PBS/Software/bioframework/scripts/Makefile
 
 # make -f $(dirname $0)/Makefile NGSDIR=$1
-make -f $makefile NGSDIR=$1
+make -f $makefile NGSDIR=$1 REF=$2
